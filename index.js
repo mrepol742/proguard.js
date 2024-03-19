cd806a375e76cce667bf58807dcc1c0fda609587
Melvin Jones Repol <mrepol742@gmail.com>
2024-03-19 08:46:01 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import fs from "fs";
 import { createHash } from "crypto";
+import js from "javascript-obfuscator";
 
 let pre = [];
 let index_f;
 
-export function scanFiles(loc) {
+export function scanFiles(loc, js_options) {
     let packagejs;
     try {
         packagejs = JSON.parse(fs.readFileSync(loc + "/package.json"));
@@ -49,18 +50,17 @@ export function scanFiles(loc) {
                 fs.mkdirSync(distPath);
                 // fs.cpSync(loc, distPath, { recursive: true }, (err) => { if (err) console.error(err); } );
 
-                let package_json_n = JSON.parse(fs.readFileSync(loc + "/package.json", "utf8"));
                 pre.find((p) => {
                     if (p.inputDir == loc && p.name == "index") {
                         index_f = p.encryptedName;
                     }
                 })
-                package_json_n.main = index_f;
-                package_json_n.scripts.start = package_json_n.scripts.start.replace("index.js", package_json_n.main);
-                package_json_n.scripts.dev = package_json_n.scripts.dev.replace("index.js", package_json_n.main);
+                packagejs.main = index_f;
+                packagejs.scripts.start = packagejs.scripts.start.replace("index.js", packagejs.main);
+                packagejs.scripts.dev = packagejs.scripts.dev.replace("index.js", packagejs.main);
                 fs.writeFileSync(
                     distPath + "/package.json",
-                    JSON.stringify(package_json_n),
+                    JSON.stringify(packagejs),
                     // see reference https://nodejs.org/api/fs.html#file-system-flags
                     { flag: "as+" }
                 );
@@ -75,9 +75,10 @@ export function scanFiles(loc) {
                             f_content = f_content.replace(new RegExp('from "(.*?)' + current_1.name + '.js"'), 'from "' + current_1.encryptedName + '.js"').replace(new RegExp('require\\("(.*?)' + current_1.name + '.js"\\)'), 'require\("' + current_1.encryptedName + '.js"\)');
                         }
                     }
+                    const result = js.obfuscate(f_content, js_options);
                     fs.writeFileSync(
                         distPath + "/" + pre[p].encryptedName + ".js",
-                        f_content, { flag: "as+" }
+                        result.getObfuscatedCode(), { flag: "as+" }
                     );
                 }
             } else {
@@ -92,5 +93,3 @@ function functionWithoutAName(loc) {
     loc[1] = loc[1] + "-dist";
     return loc.join("/");
 }
-
-scanFiles("../project-orion");
